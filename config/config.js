96f911317d8e20bcadd35f51515398fc3c7e8762
Kimmi Lee <lee.kimmixq@gmail.com>
2022-03-02 11:53:16 +0800

    Revert "completed db setup (migrations, models, seeders)"
    This reverts commit fda4ec950c2e747ea7accdc90be861b6c93d2c14.

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: 'kimmilee',
-    password: null,
-    database: 'cards_poce_development',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  }
-};
